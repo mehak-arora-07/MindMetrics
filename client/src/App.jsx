0cf16834dfe36f267828151f04716bbee1391b3d
mehak-arora-07 <mehakarora0717@gmail.com>
2026-07-25 12:54:38 +0530

    dual task empyt file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
-import WhackACircle from "./games/WhackACircle";
 import PatternSequence from "./games/PatternSequence";
-import MemoryMatrix from "./games/MemoryMatrix"
-import HiddenSymbol from "./games/HiddenSymbol";
+import MemoryMatrix from "./games/MemoryMatrix";
+import DualTask from "./games/DualTask";
+
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Route path="/login" element={<AuthPage />} />
         <Route path="/play/pattern-sequence" element={<PatternSequence />} />
         <Route path="/play/memory-matrix" element={<MemoryMatrix />} />
+        <Route path="/play/dual-task" element={<DualTask />} />
 
       </Routes>
     </BrowserRouter>
